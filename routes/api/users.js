7d40875be5cfbb3e44953814bbb22df6b27929ce
Akash Kumar <akashpk1997@gmail.com>
2020-04-21 16:23:03 +0530

    (feat) Add register user

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,5 +1,67 @@
 const express = require('express');
 const router = express.Router();
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const {check, validationResult} = require('express-validator');
+require('dotenv').config();
+
+const User = require('../../models/User');
+
+
+//POST API User
+//Register User
+
+router.post('/',
+[
+check('name', 'Please enter your name').notEmpty(),
+check('email', 'Please enter a valid email').isEmail(),
+check('password', 'Please enter a password with 6 or more character').isLength({min: 6})
+],
+ async (req, res) => {
+    const errors = validationResult(req);
+    if(!errors.isEmpty()){
+        return res.status(400).json({errors: errors.array()})
+    }
+
+    const {name, email, password} = req.body;
+
+    try {
+        let user = await User.findOne({email});
+
+        if(user) {
+            return res.status(400).json({errors: [{msg: "User already exists!"}]});
+        }
+        user = new User({
+            name,
+            email,
+            password
+        });
+        
+        const salt = await bcrypt.genSalt(10);
+
+        user.password = await bcrypt.hash(password, salt);
+
+        await user.save();
+
+        const payload = {
+            user: {
+                id: user.id
+            }
+        }
+
+        jwt.sign(payload, process.env.JWT_SECRET,
+            {expiresIn: 36000},
+            (err, token) => {
+                if(err) throw err;
+                res.json({token});
+            });
+
+    } catch (err) {
+        console.error(err.message);
+        res.send(500).send('Server Error, Please try again!');
+    }
+})
+
 
 //GET API User
 //Access level Private
