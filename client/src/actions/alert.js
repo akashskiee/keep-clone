cb06a0e8db985469fe66a9d4c95583e4c3500606
Akash Kumar <akashpk1997@gmail.com>
2020-04-27 00:09:34 +0530

    (setup) Add redux store and middleware setup

---
+++ client/src/actions/alert.js
@@ -0,0 +1,10 @@
+import {SET_ALERT, REMOVE_ALERT} from './types';
+import {v4 as uuid} from 'uuid';
+
+export const setAlert = (msg, alertType) => dispatch => {
+ const id = uuid();
+ dispatch({
+     type: SET_ALERT,
+     payload: {msg, alertType, id}
+ });
+};
