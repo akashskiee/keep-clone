a2d2de227354cd8bb784177fdd93629d1e8ac544
Akash Kumar <akashpk1997@gmail.com>
2020-04-27 21:27:11 +0530

    (feat) Add dynamic navbar, private route for homepage

--- client/src/App.js
+++ client/src/App.js
@@ -3,7 +3,7 @@ import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
 import Login from './components/auth/Login';
 import Register from './components/auth/Register';
 import Homepage from './components/layout/Homepage';
-
+import PrivateRoute from './components/routing/PrivateRoute';
 import {Provider} from 'react-redux';
 import store from './store'
 import {loadUser} from './actions/auth';
@@ -21,10 +21,10 @@ const App = () => {
     <Provider store={store}>
     <Router>
     <Fragment>
-    <Route exact path='/' component={Homepage} />
     <Switch>
     <Route exact path='/register' component={Register} />
     <Route exact path='/login' component={Login}/>
+    <PrivateRoute exact path='/' component={Homepage} />
     </Switch>
     </Fragment>
     </Router>
